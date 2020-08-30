0ea3974241fccb8b0ab5aceb004eb956cce4a1c4
Transmissions <greenfeatherhelp@gmail.com>
2020-08-29 23:15:01 -0700

    Update SlowlyLoadedList.tsx

--- src/components/shared/SlowlyLoadedList.tsx
+++ src/components/shared/SlowlyLoadedList.tsx
@@ -16,6 +16,7 @@ const useInterval = (callback: () => any, delay: number | null) => {
     }
   }, [delay]);
 };
+
 interface Props {
   length: number;
   renderItem: (index: number) => JSX.Element;
