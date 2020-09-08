818075a08fef120df3cdc77460cd3e53727c8a35
Transmissions <greenfeatherhelp@gmail.com>
2020-09-08 15:13:46 -0700

    Dont load unused contracts

--- src/context/ContractsContext.tsx
+++ src/context/ContractsContext.tsx
@@ -6,22 +6,23 @@ import { useWeb3 } from "./Web3Context";
 import FullPageSpinner from "../components/shared/FullPageSpinner";
 import Web3 from "web3";
 
-import RARI_FUND_CONTROLLER_ABI from "../static/contracts/RariFundController.json";
 import RARI_FUND_MANAGER_ABI from "../static/contracts/RariFundManager.json";
-import RARI_FUND_TOKEN_ABI from "../static/contracts/RariFundToken.json";
-
-import { RariFundController } from "../static/contracts/compiled/RariFundController";
-import { RariFundToken } from "../static/contracts/compiled/RariFundToken";
 import { RariFundManager } from "../static/contracts/compiled/RariFundManager";
+
+// import RARI_FUND_CONTROLLER_ABI from "../static/contracts/RariFundController.json";
+// import RARI_FUND_TOKEN_ABI from "../static/contracts/RariFundToken.json";
+
+// import { RariFundController } from "../static/contracts/compiled/RariFundController";
+// import { RariFundToken } from "../static/contracts/compiled/RariFundToken";
+
+// export const RARI_FUND_CONTROLLER_ADDRESS =
+//   "0x15c4ae284fbb3a6ceb41fa8eb5f3408ac485fabb";
+
+// export const RARI_FUND_TOKEN_ADDRESS =
+//   "0x9366B7C00894c3555c7590b0384e5F6a9D55659f";
 
 export const RARI_FUND_MANAGER_ADDRESS =
   "0x6bdaf490c5b6bb58564b3e79c8d18e8dfd270464";
-
-export const RARI_FUND_CONTROLLER_ADDRESS =
-  "0x15c4ae284fbb3a6ceb41fa8eb5f3408ac485fabb";
-
-export const RARI_FUND_TOKEN_ADDRESS =
-  "0x9366B7C00894c3555c7590b0384e5F6a9D55659f";
 
 export const ALL_RARI_FUND_MANAGER_ADDRESSES = [
   RARI_FUND_MANAGER_ADDRESS,
@@ -29,9 +30,9 @@ export const ALL_RARI_FUND_MANAGER_ADDRESSES = [
 ];
 
 export interface ContractsContextData {
-  RariFundController: RariFundController;
+  // RariFundController: RariFundController;
+  // RariFundToken: RariFundToken;
   RariFundManager: RariFundManager;
-  RariFundToken: RariFundToken;
 }
 
 export const ContractsContext = React.createContext<
@@ -55,17 +56,17 @@ export const ContractsProvider = ({ children }: { children: JSX.Element }) => {
     }
 
     setContractData({
-      RariFundController: new web3.eth.Contract(
-        RARI_FUND_CONTROLLER_ABI as AbiItem[],
-        RARI_FUND_CONTROLLER_ADDRESS
-      ) as any,
+      // RariFundController: new web3.eth.Contract(
+      //   RARI_FUND_CONTROLLER_ABI as AbiItem[],
+      //   RARI_FUND_CONTROLLER_ADDRESS
+      // ) as any,
+      // RariFundToken: new web3.eth.Contract(
+      //   RARI_FUND_TOKEN_ABI as AbiItem[],
+      //   RARI_FUND_TOKEN_ADDRESS
+      // ) as any,
       RariFundManager: new web3.eth.Contract(
         RARI_FUND_MANAGER_ABI as AbiItem[],
         RARI_FUND_MANAGER_ADDRESS
-      ) as any,
-      RariFundToken: new web3.eth.Contract(
-        RARI_FUND_TOKEN_ABI as AbiItem[],
-        RARI_FUND_TOKEN_ADDRESS
       ) as any,
     });
   }, [isAuthed, web3Authed, web3Network]);
