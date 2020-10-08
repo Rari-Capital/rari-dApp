ff98ce95ab3c8a4deb2b4f5fa61491ed37e9d596
Transmissions <greenfeatherhelp@gmail.com>
2020-10-08 08:34:33 -0700

    Update E2E.spec.js

--- cypress/e2e/E2E.spec.js
+++ cypress/e2e/E2E.spec.js
@@ -1,13 +1,12 @@
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
 
-describe("E2E", function () {
+describe("E2E", function() {
   before(() => {
     cy.visit("/");
   });
 
-  it("renders the home page", () => {
-    cy.findByText(/Connect Wallet/i).should("be.visible");
-    cy.findByText(/Get Wallet/i).should("be.visible");
+  it("renders the pool page", () => {
+    cy.findByText(/Latest Rari News/i).should("be.visible");
   });
 });
