020e6e7dba5b412294aab78e586f0ca6b083375a
Transmissions <greenfeatherhelp@gmail.com>
2020-10-09 15:40:47 -0700

    Typescript fixes for api

--- api/news.ts
+++ api/news.ts
@@ -4,10 +4,10 @@ import Twitter from "twitter-lite";
 
 export default async (request: NowRequest, response: NowResponse) => {
   const user = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.TWITTER_CONSUMER_KEY!,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET!,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY!,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
   });
 
   const timeline = await user.get("statuses/user_timeline", {
@@ -15,10 +15,10 @@ export default async (request: NowRequest, response: NowResponse) => {
     count: 3,
   });
 
-  let tweets = [];
+  let tweets: string[] = [];
 
-  timeline.forEach((tweet) => {
-    tweets.push(tweet.text);
+  timeline.forEach(({ text }: { text: string }) => {
+    tweets.push(text);
   });
 
   response.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
