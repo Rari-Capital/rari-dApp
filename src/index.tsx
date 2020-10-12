ffac4f8344baeffd20123180a15b2c77da308414
Transmissions <greenfeatherhelp@gmail.com>
2020-10-12 08:50:58 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-import * as serviceWorker from "./serviceWorker";
-
 import "./index.css";
 
 // Remove this ignore when TypeScript PR gets merged.
@@ -73,5 +71,3 @@ ReactDOM.render(
   </>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
