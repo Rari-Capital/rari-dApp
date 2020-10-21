bfe50a0da950fc9e64abd03aa4291b0451a98621
davidlucid <davidlucidsl@gmail.com>
2020-10-21 17:28:58 -0500

    Single fix to pools.stable

--- src/rari-sdk/pools/stable.js
+++ src/rari-sdk/pools/stable.js
@@ -1827,7 +1827,7 @@ export default class StablePool {
             if (allBalances["0"][i] !== currencyCode) {
               var rawFundBalanceBN = Web3.utils.toBN(0);
               for (var j = 0; j < allBalances["3"][i].length; j++)
-                rawFundBalanceBN.iadd(allBalances["3"][i][j]);
+                rawFundBalanceBN.iadd(Web3.utils.toBN(allBalances["3"][i][j]));
               if (rawFundBalanceBN.gt(Web3.utils.toBN(0)))
                 inputCandidates.push({
                   currencyCode: allBalances["0"][i],
