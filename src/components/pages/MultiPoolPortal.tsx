1e329e7d210c7cefb09eaeb7d3713a0a9cbe115e
Transmissions <greenfeatherhelp@gmail.com>
2020-12-10 11:46:22 -0800

    Update MultiPoolPortal.tsx

--- src/components/pages/MultiPoolPortal.tsx
+++ src/components/pages/MultiPoolPortal.tsx
@@ -113,7 +113,7 @@ const MultiPoolPortal = React.memo(() => {
         width={columnWidth}
         px={columnWidth === "100%" ? DASHBOARD_BOX_SPACING.asPxString() : 0}
       >
-        <Header isAuthed={isAuthed} lessLinks={columnWidth === "700px"} />
+        <Header isAuthed={isAuthed} />
 
         <FundStats />
 
