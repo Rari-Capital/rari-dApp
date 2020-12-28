39e1eff39b00c471c470b89a3fea0923b4962686
t11s <greenfeatherhelp@gmail.com>
2020-12-27 17:45:41 -0800

    Update SaffronContext.tsx

--- src/components/pages/Tranches/SaffronContext.tsx
+++ src/components/pages/Tranches/SaffronContext.tsx
@@ -13,8 +13,9 @@ export const SaffronContext = React.createContext<
   SaffronContextType | undefined
 >(undefined);
 
-const SaffronStrategyAddress = "0x9e0278646fD72318909338Ad87deC7f3464BC434";
-const SaffronPoolAddress = "0xbafA231AAac12CE8ba0b23b86669f54a05fC23b5";
+const SaffronStrategyAddress_EPOCH_4 =
+  "0x9e0278646fD72318909338Ad87deC7f3464BC434";
+const SaffronPoolAddress_EPOCH_4 = "0xbafA231AAac12CE8ba0b23b86669f54a05fC23b5";
 
 export const SaffronProvider = React.memo(({ children }) => {
   const { rari } = useRari();
@@ -22,14 +23,14 @@ export const SaffronProvider = React.memo(({ children }) => {
   const [saffronStrategy, setSaffronStrategy] = useState(() => {
     return new rari.web3.eth.Contract(
       SaffronStrategyABI as any,
-      SaffronStrategyAddress
+      SaffronStrategyAddress_EPOCH_4
     );
   });
 
   const [saffronPool, setSaffronPool] = useState(() => {
     return new rari.web3.eth.Contract(
       SaffronPoolABI as any,
-      SaffronPoolAddress
+      SaffronPoolAddress_EPOCH_4
     );
   });
 
@@ -37,12 +38,15 @@ export const SaffronProvider = React.memo(({ children }) => {
     setSaffronStrategy(
       new rari.web3.eth.Contract(
         SaffronStrategyABI as any,
-        SaffronStrategyAddress
+        SaffronStrategyAddress_EPOCH_4
       )
     );
 
     setSaffronPool(
-      new rari.web3.eth.Contract(SaffronPoolABI as any, SaffronPoolAddress)
+      new rari.web3.eth.Contract(
+        SaffronPoolABI as any,
+        SaffronPoolAddress_EPOCH_4
+      )
     );
   }, [rari]);
 
