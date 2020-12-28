a4ecfe87b9b07233ea039e5f10093caff77ccf11
t11s <greenfeatherhelp@gmail.com>
2020-12-27 17:56:20 -0800

    Dai TVL

--- src/hooks/useTVL.ts
+++ src/hooks/useTVL.ts
@@ -3,16 +3,23 @@ import { useRari } from "../context/RariContext";
 import Rari from "../rari-sdk/index";
 
 export const fetchTVL = async (rari: Rari) => {
-  const [stableTVL, yieldTVL, ethTVLInETH, ethPriceBN] = await Promise.all([
+  const [
+    stableTVL,
+    yieldTVL,
+    ethTVLInETH,
+    daiTVL,
+    ethPriceBN,
+  ] = await Promise.all([
     rari.pools.stable.balances.getTotalSupply(),
     rari.pools.yield.balances.getTotalSupply(),
     rari.pools.ethereum.balances.getTotalSupply(),
+    rari.pools.dai.balances.getTotalSupply(),
     rari.getEthUsdPriceBN(),
   ]);
 
   const ethTVL = ethTVLInETH.mul(ethPriceBN.div(rari.web3.utils.toBN(1e18)));
 
-  return stableTVL.add(yieldTVL).add(ethTVL);
+  return stableTVL.add(yieldTVL).add(ethTVL).add(daiTVL);
 };
 
 export const useTVLFetchers = () => {
