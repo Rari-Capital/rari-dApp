bd56c6552250d3fda150a960be478590faa65532
t11s <greenfeatherhelp@gmail.com>
2021-01-22 10:14:34 -0800

    Update AccountButton.tsx

--- src/components/shared/AccountButton.tsx
+++ src/components/shared/AccountButton.tsx
@@ -9,7 +9,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 
-import { Row, Column, Center, useIsMobile } from "buttered-chakra";
+import { Row, Column, Center } from "buttered-chakra";
 import DashboardBox from "./DashboardBox";
 
 // @ts-ignore
@@ -26,6 +26,7 @@ import { ClaimRGTModal } from "./ClaimRGTModal";
 import { version } from "../..";
 
 import MoonpayModal from "../pages/MoonpayModal";
+import { useIsSmallScreen } from "../../hooks/useIsSmallScreen";
 
 export const AccountButton = React.memo(() => {
   const {
@@ -81,7 +82,7 @@ const Buttons = ({
 
   const { t } = useTranslation();
 
-  const isMobile = useIsMobile();
+  const isMobile = useIsSmallScreen();
 
   return (
     <>
@@ -97,6 +98,7 @@ const Buttons = ({
           >
             <Center expand>{t("Buy Crypto")}</Center>
           </DashboardBox>
+
           <DashboardBox
             ml={4}
             as="button"
