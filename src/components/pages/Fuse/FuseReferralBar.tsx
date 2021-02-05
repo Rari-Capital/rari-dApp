772fd46df5a6e8978825a083880d26315f95114d
Andreas Bigger <abigger87@gmail.com>
2021-02-05 07:44:58 -0800

    fix styling

--- src/components/pages/Fuse/FuseReferralBar.tsx
+++ src/components/pages/Fuse/FuseReferralBar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useIsSmallScreen } from "../../../hooks/useIsSmallScreen";
 import DashboardBox from "../../shared/DashboardBox";
-import { shortAddress } from "../../../utils/shortAddress";
+import { mediumAddress } from "../../../utils/shortAddress";
 import { useRari } from "../../../context/RariContext";
 
 const FuseReferralBar = () => {
@@ -12,9 +12,10 @@ const FuseReferralBar = () => {
     let { poolId } = useParams();
     const { address } = useRari();
 
-    const short_referral_code = `https://app.rari.capital/fuse/pool/${poolId}/${shortAddress(address)}`;
+    const short_referral_code = `https://app.rari.capital/fuse/pool/${poolId}/${mediumAddress(address)}`;
     const full_referral_code = `https://app.rari.capital/fuse/pool/${poolId}/${address}`;
 
+    let width = !isMobile ? "50%" : "100%";
     return (
         <RowOrColumn
             isRow={!isMobile}
@@ -23,31 +24,34 @@ const FuseReferralBar = () => {
             crossAxisAlignment="center"
             py={4}
             >
-            <DashboardBox>
-                <RowOrColumn
-                    isRow={!isMobile}
-                    expand
-                    mainAxisAlignment="flex-end"
-                    crossAxisAlignment="center"
-                    p={2}
-                    >
+            <div style={{ width: width, paddingLeft: "0.5rem"}}>
+                <DashboardBox>
                     <RowOrColumn
-                        isRow={false}
-                        mainAxisAlignment="flex-start"
+                        isRow={true}
+                        expand
+                        mainAxisAlignment="space-between"
                         crossAxisAlignment="center"
+                        width="100%"
+                        p={2}
                         >
-                        <Center margin={"0 auto 0 0"} pl="9px" pr="10px">
-                            Earn crypto with your referral link:
-                        </Center>
-                        <Center margin={"0 auto 0 0"} pl="9px" pr="10px">
-                            {short_referral_code}
-                        </Center>
+                        <RowOrColumn
+                            isRow={false}
+                            mainAxisAlignment="flex-start"
+                            crossAxisAlignment="center"
+                            >
+                            <Center margin={"0 auto 0 0"} pl="9px" pr="10px">
+                                Earn crypto with your referral link:
+                            </Center>
+                            <Center margin={"0 auto 0 0"} pl="9px" pr="10px">
+                                {short_referral_code}
+                            </Center>
+                        </RowOrColumn>
+                        <Button background={"none"} color={"aliceblue"} _hover={{ bg: "none", color: "aliceblue" }} onClick={() =>  navigator.clipboard.writeText(full_referral_code)}>
+                            Copy
+                        </Button>
                     </RowOrColumn>
-                    <Button background={"none"} color={"aliceblue"} _hover={{ bg: "none", color: "aliceblue" }} onClick={() =>  navigator.clipboard.writeText(full_referral_code)}>
-                        Copy
-                    </Button>
-                </RowOrColumn>
-            </DashboardBox>
+                </DashboardBox>
+            </div>
         </RowOrColumn>
     );
 };
