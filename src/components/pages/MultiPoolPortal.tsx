7e969fd0aaf41f3ce46b34b9095e55d3fa43b2c3
t11s <greenfeatherhelp@gmail.com>
2021-03-18 15:20:03 -0700

    News

--- src/components/pages/MultiPoolPortal.tsx
+++ src/components/pages/MultiPoolPortal.tsx
@@ -613,6 +613,8 @@ export const NewsAndTwitterLink = () => {
 
 const NewsMarquee = React.memo(() => {
   const news = [
+    "The first Fuse pools deployed by the Rari Capital DAO are now open for deposits/borrows in the Fuse tab!",
+    "You can now earn rewards for pooling ETH and RGT on Sushiswap in the Pool2 tab.",
     "Saffron x Rari tranches are now open for deposits under the Tranches tab.",
     "We're migrating from Telegram to Discord! Join us there to talk all things Rari Capital.",
     "Individual Pool Dashboards are now live! View detailed analytics about your account and other useful metrics!",
