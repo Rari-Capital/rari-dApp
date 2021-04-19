78919d92eeccff909946f6dc1fb5b6d76d9a968b
Sharad Shekar <sharadshekar@gmail.com>
2021-04-18 19:53:08 -0500

    wip table

--- src/components/pages/Stats/StatsEarnSection.tsx
+++ src/components/pages/Stats/StatsEarnSection.tsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React from 'react';
 import {
-    Heading,
-  } from '@chakra-ui/react';
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from '@chakra-ui/react';
 
 export default () => {
-    return (
-        <>
-        <Heading>
-            Earn Section baby
-        </Heading> 
-        </>
-    )
-}
-
+  return (
+    <>
+      <Heading>Earn Section baby</Heading>
+      <Table variant="simple">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric>multiply by</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>inches</Td>
+            <Td>millimetres (mm)</Td>
+            <Td isNumeric>25.4</Td>
+          </Tr>
+          <Tr>
+            <Td>feet</Td>
+            <Td>centimetres (cm)</Td>
+            <Td isNumeric>30.48</Td>
+          </Tr>
+          <Tr>
+            <Td>yards</Td>
+            <Td>metres (m)</Td>
+            <Td isNumeric>0.91444</Td>
+          </Tr>
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric>multiply by</Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </>
+  );
+};
