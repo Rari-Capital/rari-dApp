8f15fc26948bd4c23078c89948f854b148c82f00
t11s <greenfeatherhelp@gmail.com>
2021-04-23 22:39:57 -0700

    Per Pool TVL

--- api/stats.ts
+++ api/stats.ts
@@ -7,7 +7,7 @@ import {
   initFuseWithProviders,
 } from "../src/utils/web3Providers";
 
-import { fetchTVL } from "../src/utils/fetchTVL";
+import { perPoolTVL } from "../src/utils/fetchTVL";
 import {
   fetchDAIPoolAPY,
   fetchPoolAPY,
@@ -18,24 +18,26 @@ import { Pool } from "../src/utils/poolUtils";
 const rari = new Rari(turboGethURL);
 const fuse = initFuseWithProviders();
 
+const mantissaToFloat = (BN: any) => {
+  return parseFloat(rari.web3.utils.fromWei(BN));
+};
+
 export default async (request: NowRequest, response: NowResponse) => {
   const [
-    rawTVL,
+    tvls,
     rawStablePoolAPY,
     rawYieldPoolAPY,
     rawEthPoolAPY,
     rawDaiPoolAPY,
     rawRgtAPR,
   ] = await Promise.all([
-    fetchTVL(rari, fuse),
+    perPoolTVL(rari, fuse),
     fetchPoolAPY(rari, Pool.STABLE),
     fetchPoolAPY(rari, Pool.YIELD),
     fetchPoolAPY(rari, Pool.ETH),
     fetchDAIPoolAPY(rari),
     fetchRGTAPR(rari),
   ]);
-
-  const tvl = parseFloat(rari.web3.utils.fromWei(rawTVL));
 
   const stablePoolAPY = parseFloat(rawStablePoolAPY);
   const yieldPoolAPY = parseFloat(rawYieldPoolAPY);
@@ -48,7 +50,22 @@ export default async (request: NowRequest, response: NowResponse) => {
   response.setHeader("Cache-Control", "s-maxage=600");
 
   response.json({
-    tvl,
+    tvl: mantissaToFloat(
+      tvls.stableTVL
+        .add(tvls.yieldTVL)
+        .add(tvls.ethTVL)
+        .add(tvls.daiTVL)
+        .add(tvls.stakedTVL)
+        .add(tvls.fuseTVL)
+    ),
+
+    stableTVL: mantissaToFloat(tvls.stableTVL),
+    yieldTVL: mantissaToFloat(tvls.yieldTVL),
+    ethTVL: mantissaToFloat(tvls.ethTVL),
+    daiTVL: mantissaToFloat(tvls.daiTVL),
+    stakedTVL: mantissaToFloat(tvls.stakedTVL),
+    fuseTVL: mantissaToFloat(tvls.fuseTVL),
+    ///////////
     rgtAPR,
     stablePoolAPY,
     ethPoolAPY,
