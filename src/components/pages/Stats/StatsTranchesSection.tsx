7d0c5a7facbf09c0d64fac6830838f2d62d3272c
sharad-s <sharadshekar@gmail.com>
2021-04-26 11:32:23 -0500

    Tranches

--- src/components/pages/Stats/StatsTranchesSection.tsx
+++ src/components/pages/Stats/StatsTranchesSection.tsx
@@ -8,42 +8,26 @@ import {
     Tr,
     Th,
     Td,
-    Avatar,
 } from '@chakra-ui/react';
 import { motion } from 'framer-motion'
-import { QuestionOutlineIcon } from '@chakra-ui/icons'
 
 // Hooks
-import { usePool2APR } from 'hooks/pool2/usePool2APR';
-import { usePool2UnclaimedRGT } from 'hooks/pool2/usePool2UnclaimedRGT';
-import { usePool2Balance } from 'hooks/pool2/usePool2Balance';
-import { SimpleTooltip } from 'components/shared/SimpleTooltip';
+import { useTokenData } from "hooks/useTokenData";
 import { Row } from 'buttered-chakra';
+import { useMySaffronData, usePrincipal, TranchePool, TrancheRating, useEstimatedSFI, usePrincipalBalance } from 'hooks/tranches/useSaffronData';
 import { smallUsdFormatter } from 'utils/bigUtils';
-import { useMySaffronData, usePrincipal, TranchePool, TrancheRating, useEstimatedSFI } from 'hooks/tranches/useSaffronData';
-import { useSFIDistributions } from 'hooks/tranches/useSFIDistributions';
-import { useSFIEarnings } from 'hooks/tranches/useSFIEarnings';
 
 const Earn = () => {
 
-    const apr = usePool2APR()
-    const earned = usePool2UnclaimedRGT()
-    const balance = usePool2Balance()
+    const dai = useTokenData('0x6b175474e89094c44da98b954eedeac495271d0f')
 
     const mySaffronData = useMySaffronData()
     const daiSPrincipal = usePrincipal(TranchePool.DAI, TrancheRating.S)
     const daiAPrincipal = usePrincipal(TranchePool.DAI, TrancheRating.A)
     const estimatedSFI = useEstimatedSFI()
-    const sfiDistributions = useSFIDistributions()
-    const sfiEarnings = useSFIEarnings()
-
-    console.log({sfiDistributions, sfiEarnings})
-
-    const totalPrincipal = parseFloat(daiSPrincipal ?? '0') + parseFloat(daiAPrincipal ?? '0')
-
+    const totalPrincipalFormatted = usePrincipalBalance()
+    const totalPrincipal: number = totalPrincipalFormatted ? parseFloat(totalPrincipalFormatted?.replace(",", "").replace("$", "")) : 0
     const hasDeposits = useMemo(() => totalPrincipal > 0, [totalPrincipal])
-
-    // console.log({  mySaffronData, totalPrincipal, daiSPrincipal, daiAPrincipal })
 
     return (
         <motion.div
@@ -65,47 +49,51 @@ const Earn = () => {
                 </Thead>
                 <Tbody>
                     <>
-                    {/* DAI S Pool */}
-                        <Tr>
-                            <Td>
-                                <Row mainAxisAlignment="flex-start" crossAxisAlignment="center">
-                                    <Box>
-                                        <Text textAlign="right"> DAI-S  </Text>
-                                    </Box>
-                                </Row>
 
-                            </Td>
-                            <Td><Text textAlign="right">{mySaffronData?.[0]?.tranches?.[TrancheRating.S]?.['total-apy']}%</Text></Td>
-                            <Td>
-                                <Text textAlign="right">{daiSPrincipal} DAI</Text>
-                            </Td>
-                            <Td> <Text textAlign="right">{estimatedSFI?.formattedSPoolSFIEarned}</Text> </Td>
-                            <Td><Text textAlign="right">N/A</Text></Td>
-                        </Tr>
-                    {/* DAI A Pool */}
-                         <Tr>
-                            <Td>
-                                <Row mainAxisAlignment="flex-start" crossAxisAlignment="center">
-                                    <Box>
-                                        <Text textAlign="right"> DAI-A  </Text>
-                                    </Box>
-                                </Row>
+                        {/* DAI S Pool */}
+                        {hasDeposits && (
+                            <>
+                                <Tr>
+                                    <Td>
+                                        <Row mainAxisAlignment="flex-start" crossAxisAlignment="center">
+                                            <Box>
+                                                <Text textAlign="right"> DAI-S  </Text>
+                                            </Box>
+                                        </Row>
 
-                            </Td>
-                            <Td><Text textAlign="right">{mySaffronData?.[0]?.tranches?.[TrancheRating.A]?.['total-apy']}%</Text></Td>
-                            <Td>
-                                <Text textAlign="right">{daiAPrincipal} DAI</Text>
-                            </Td>
-                            <Td> <Text textAlign="right">{estimatedSFI?.formattedAPoolSFIEarned}</Text> </Td>
-                            <Td><Text textAlign="right">N/A</Text></Td>
-                        </Tr>
-                    {/* Totals */}
+                                    </Td>
+                                    <Td><Text textAlign="right">{mySaffronData?.[0]?.tranches?.[TrancheRating.S]?.['total-apy']}%</Text></Td>
+                                    <Td>
+                                        <Text textAlign="right">{daiSPrincipal} DAI</Text>
+                                    </Td>
+                                    <Td> <Text textAlign="right">{estimatedSFI?.formattedSPoolSFIEarned}</Text> </Td>
+                                    <Td><Text textAlign="right">N/A</Text></Td>
+                                </Tr>
+                                <Tr>
+                                    <Td>
+                                        <Row mainAxisAlignment="flex-start" crossAxisAlignment="center">
+                                            <Box>
+                                                <Text textAlign="right"> DAI-A  </Text>
+                                            </Box>
+                                        </Row>
+
+                                    </Td>
+                                    <Td><Text textAlign="right">{mySaffronData?.[0]?.tranches?.[TrancheRating.A]?.['total-apy']}%</Text></Td>
+                                    <Td>
+                                        <Text textAlign="right">{daiAPrincipal} DAI</Text>
+                                    </Td>
+                                    <Td> <Text textAlign="right">{estimatedSFI?.formattedAPoolSFIEarned}</Text> </Td>
+                                    <Td><Text textAlign="right">N/A</Text></Td>
+                                </Tr>
+                            </>
+                        )}
+                        {/* Totals */}
                         <Tr>
                             <Td><Text fontWeight={hasDeposits && "bold"}>Total</Text></Td>
                             <Td><Text fontWeight={hasDeposits && "bold"} textAlign="right"></Text></Td>
-                            <Td><Text fontWeight={hasDeposits && "bold"} textAlign="right">{smallUsdFormatter(totalPrincipal)}</Text></Td>
-                            <Td><Text fontWeight={hasDeposits && "bold"} textAlign="right">{estimatedSFI?.formattedTotalSFIEarned}</Text></Td>
-                            <Td></Td>
+                            <Td><Text fontWeight={hasDeposits && "bold"} textAlign="right">{smallUsdFormatter(totalPrincipal) ?? 0 }</Text></Td>
+                            <Td><Text fontWeight={hasDeposits && "bold"} textAlign="right">{estimatedSFI?.formattedTotalSFIEarned ?? '0 SFI'}</Text></Td>
+                            <Td> <Text fontWeight={hasDeposits && "bold"} textAlign="right"></Text></Td>
                         </Tr>
                     </>
                 </Tbody>
