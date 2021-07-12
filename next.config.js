e0d26568eed4f2ac601765864848a5164415c90e
sharad-s <sharadshekar@gmail.com>
2021-07-12 02:29:29 -0500

    locales test

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   /* config options here */
   images: {
     domains: ["raw.githubusercontent.com"],
+    i18n: {
+      defaultLocale: 'en',
+      locales: ['en', 'zh-TW', 'zh-CN'],
+    },
   },
 };
