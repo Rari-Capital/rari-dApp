eda4eca0fe4767a2eebb60eaaf76fe2618ef21a7
sharad-s <sharadshekar@gmail.com>
2021-07-12 18:10:31 -0500

    add localePath to i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "zh-TW", "zh-CN"],
+    localePath: path.resolve('./public/locales')
   }
 };
