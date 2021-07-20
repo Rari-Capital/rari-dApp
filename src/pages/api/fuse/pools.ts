fbc473ce140fc9474e04f15cf2cff5857acbf674
sharad-s <sharadshekar@gmail.com>
2021-07-19 20:20:04 -0500

    GET all fuse pools endpt

--- src/pages/api/fuse/pools.ts
+++ src/pages/api/fuse/pools.ts
@@ -10,6 +10,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
+  let _poolIndices: string[] = [];
+
   if (req.method === "POST") {
     try {
       let userAddress: string = "";
@@ -30,7 +32,6 @@ export default async function handler(
       const fuse = initFuseWithProviders(turboGethURL);
 
       // If we specified poolIndices, use those, else fetch all pools
-      let _poolIndices: string[] = [];
       if (poolIndices?.length) {
         _poolIndices = poolIndices;
       } else {
@@ -49,9 +50,52 @@ export default async function handler(
         )
       );
 
-      return res.status(200).json({ fusePoolsData, _poolIndices, userAddress });
+      return res.status(200).json({ pools: fusePoolsData, userAddress });
     } catch (error) {
       console.log({ error });
+      return res.status(400).json({ error });
+    }
+  }
+  //   Gets ALL fuse pool Data
+  else if (req.method === "GET") {
+    try {
+      let userAddress: string = "";
+
+      // Validate queryParams
+      // const poolIndices = req.body.poolIndices as string[]; // optional
+      const address = (req.body.address as string) ?? EmptyAddress; // optional
+
+      try {
+        userAddress = Web3.utils.toChecksumAddress(address);
+      } catch (err) {
+        return res.status(400).json({ error: "Invalid address provided." });
+      }
+
+      // Set up SDKs
+      const web3 = new Web3(turboGethURL);
+      const rari = new Rari(web3);
+      const fuse = initFuseWithProviders(turboGethURL);
+
+      const fusePools = await fetchPools({
+        rari,
+        fuse,
+        address: userAddress,
+        filter: "",
+      });
+
+      const poolIndices = fusePools
+        .filter((pool) => !!pool.underlyingTokens.length) // filter out empty pools
+        .map((pool) => pool.id.toString());
+
+      const fusePoolsData = await Promise.all(
+        poolIndices.map((poolIndex) =>
+          fetchFusePoolData(poolIndex, userAddress, fuse, rari)
+        )
+      );
+
+      return res.status(200).json({ pools: fusePoolsData, userAddress });
+    } catch (error) {
+      console.log({ error }, error.message);
       return res.status(400).json({ error });
     }
   }
