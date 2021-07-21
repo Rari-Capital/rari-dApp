6098911dc84b32f092dc973455b6cdc841e3ac71
t11s <greenfeatherhelp@gmail.com>
2021-07-21 11:41:01 -0700

    Update web3Providers.ts

--- src/utils/web3Providers.ts
+++ src/utils/web3Providers.ts
@@ -1,6 +1,6 @@
 import Fuse from "../fuse-sdk/src";
 
-export const infuraURL = `https://mainnet.infura.io/v3/2e56500614ce4496bde29b22e65f1607`;
+export const infuraURL = `https://mainnet.infura.io/v3/bd74c68478354521921a1ac57614862b`;
 export const turboGethURL = `https://turbogeth.crows.sh`;
 
 export function chooseBestWeb3Provider() {
