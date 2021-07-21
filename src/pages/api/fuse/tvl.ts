2bf4cc1f45808d934fd972247e1d89f96b8037a0
sharad-s <sharadshekar@gmail.com>
2021-07-20 22:14:13 -0500

    cors fix hopefully

--- src/pages/api/fuse/tvl.ts
+++ src/pages/api/fuse/tvl.ts
@@ -6,6 +6,28 @@ import Rari from "lib/rari-sdk";
 
 import { APIError } from "../accounts/fuse/balances";
 
+// Cors
+import Cors from "cors";
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD", "OPTIONS"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: any, res: any, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 export interface APIFuseTVL {
   totalSuppliedUSD: number;
   totalBorrowedUSD: number;
@@ -15,6 +37,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<APIFuseTVL | APIError>
 ) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   //   Gets ALL fuse pool Data
   if (req.method === "GET") {
     try {
@@ -24,7 +49,6 @@ export default async function handler(
       const fuse = initFuseWithProviders(turboGethURL);
 
       //   Get TVL supplied and borrowed in ETH denomination
-
       const [{ totalSuppliedETH, totalBorrowedETH }, ethUSDPriceBN] =
         await Promise.all([
           fetchFuseTVLBorrowsAndSupply(fuse),
