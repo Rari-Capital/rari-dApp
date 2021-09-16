cc62827cd295d012519082ac6ac74baca94a0588
t11s <greenfeatherhelp@gmail.com>
2021-09-16 11:03:26 -0700

    Name fix

--- src/utils/web3Providers.ts
+++ src/utils/web3Providers.ts
@@ -1,10 +1,10 @@
 import Fuse from "../fuse-sdk/src";
 
-export const turboGethURL = `https://eth-mainnet.alchemyapi.io/v2/2Mt-6brbJvTA4w9cpiDtnbTo6qOoySnN`;
+export const alchemyURL = `https://eth-mainnet.alchemyapi.io/v2/2Mt-6brbJvTA4w9cpiDtnbTo6qOoySnN`;
 
 export function chooseBestWeb3Provider() {
   if (typeof window === "undefined") {
-    return turboGethURL;
+    return alchemyURL;
   }
 
   if (window.ethereum) {
@@ -12,7 +12,7 @@ export function chooseBestWeb3Provider() {
   } else if (window.web3) {
     return window.web3.currentProvider;
   } else {
-    return turboGethURL;
+    return alchemyURL;
   }
 }
 
@@ -20,7 +20,7 @@ export const initFuseWithProviders = (provider = chooseBestWeb3Provider()) => {
   const fuse = new Fuse(provider);
 
   // @ts-ignore We have to do this to avoid Infura ratelimits on our large calls.
-  fuse.contracts.FusePoolLens.setProvider(turboGethURL);
+  fuse.contracts.FusePoolLens.setProvider(alchemyURL);
 
   return fuse;
 };
