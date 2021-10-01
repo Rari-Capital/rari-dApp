c322e9002dd5c3b86ff5039a708c771c96f158ff
sharad-s <sharadshekar@gmail.com>
2021-10-01 17:15:55 -0500

    update testnet provider

--- src/utils/web3Providers.ts
+++ src/utils/web3Providers.ts
@@ -1,6 +1,7 @@
 import Fuse from "../fuse-sdk/src";
 
 export const alchemyURL =  `https://eth-mainnet.alchemyapi.io/v2/2Mt-6brbJvTA4w9cpiDtnbTo6qOoySnN`;
+export const testnetURL =  `http://localhost:8546`;
 
 export function chooseBestWeb3Provider() {
   if (typeof window === "undefined") {
@@ -17,10 +18,10 @@ export function chooseBestWeb3Provider() {
 }
 
 export const initFuseWithProviders = (provider = chooseBestWeb3Provider()) => {
-  const fuse = new Fuse(provider);
+  const fuse = new Fuse(testnetURL);
 
   // @ts-ignore We have to do this to avoid Infura ratelimits on our large calls.
-  fuse.contracts.FusePoolLens.setProvider(alchemyURL);
+  fuse.contracts.FusePoolLens.setProvider(testnetURL);
 
   return fuse;
 };
