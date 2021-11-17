4904832521695ceafd4f12150b9f78386c2445a3
davidlucid <davidlucidsl@gmail.com>
2021-11-17 16:05:26 -0500

    New price oracle contract addresses and runtime bytecode hashes

--- src/fuse-sdk/src/index.js
+++ src/fuse-sdk/src/index.js
@@ -82,7 +82,7 @@ export default class Fuse {
     UniswapV3TwapPriceOracleV2_Uniswap_10000_USDC:
       "0x3288a2d5f11FcBefbf77754e073cAD2C10325dE2",
     // RecursivePriceOracle: "", // TODO: Set correct mainnet address after deployment
-    YVaultV1PriceOracle: "0xb04be6165cf1879310e48f8900ad8c647b9b5c5d", // NOT CURRENTLY IN USE
+    YVaultV1PriceOracle: "0xb5e8e42639e20285c9e58a317c28d9a4d7cb7000", // v1.2.1; v1.0.0 = 0xb04be6165cf1879310e48f8900ad8c647b9b5c5d
     YVaultV2PriceOracle: "0xb669d0319fb9de553e5c206e6fbebd58512b668b",
     // AlphaHomoraV1PriceOracle: "", // TODO: Set correct mainnet address after deployment
     // AlphaHomoraV2PriceOracle: "", // TODO: Set correct mainnet address after deployment
@@ -104,12 +104,17 @@ export default class Fuse {
       "0x9ee412a83a52f033d23a0b7e2e030382b3e53208", // v1.1.3
     UniswapTwapPriceOracleV2_Uniswap_FRAX:
       "0x6127e381756796fb978bc872556bf790f14cde98", // v1.1.3
+    UniswapTwapPriceOracleV2_SushiSwap_ETH:
+      "0xf411CD7c9bC70D37f194828ce71be00d9aEC9edF", // v1.1.3
     SushiBarPriceOracle: "0x290E0f31e96e13f9c0DB14fD328a3C2A94557245",
     BadgerPriceOracle: "0xd0C86943e594640c4598086a2359A0e70b80eF8D",
-    HarvestPriceOracle: "0x8D364609cd2716172016838fF9FBC7fBcAC91792",
+    HarvestPriceOracle: "0x6141d9353bb1fb8131d07d358c112b372aa92514", // v1.2.1; v1.1.4 = 0x8D364609cd2716172016838fF9FBC7fBcAC91792
     StakedSdtPriceOracle: "0x5447c825ee330015418c1a0d840c4a1b5a7176cc",
     TokemakPoolTAssetPriceOracle: "0xd806782b31EC52FcB7f2a009d7D045bB732431Fb",
     MStablePriceOracle: "0xeb988f5492C86584f8D8f1B8662188D5A9BfE357",
+    GelatoGUniPriceOracle: "0xea3633b38c747cea231adb74b511dc2ed3992b43",
+    StakedSpellPriceOracle: "0xb544f62045b96a60b398abb5a5c23bf04cb4ed9c",
+    CurveTriCryptoLpTokenPriceOracle: "0xb2d16916d520d585ee49f08db1436b961b48fe60",
   };
 
   static UNISWAP_TWAP_PRICE_ORACLE_ROOT_CONTRACT_ADDRESS =
@@ -144,21 +149,31 @@ export default class Fuse {
       "0xa2537dcbd2b55b1a690db3b83fa1042f86b21ec3e1557f918bc3930b6bbb9244",
     UniswapTwapPriceOracle_SushiSwap:
       "0x9b11abfe7bfc1dcef0b1bc513959f1172cfe2cb595c5131b9cabc3b6448d89ac",
+    UniswapLpTokenPriceOracle:
+      "0xbcddb66e4e9c038b4ee1cf4caf1e8c8119225d72a8407fc32caa1988e4a7fe31",
     UniswapV3TwapPriceOracle_Uniswap_3000:
       "0xb300f7f64110b952340e896d33f133482de6715f1b8b7e0acbd2416e0e6593c1",
+    UniswapV3TwapPriceOracle_Uniswap_10000:
+      "0xef237fadaffff8a1b5daa4d448c7935cf0f71e2ee01a53856bb0d7884b0ad78c",
+    UniswapV3TwapPriceOracleV2_Uniswap_500_USDC:
+      "0xaaba60b3af593a8ecde61d8516ad0353db8cc2777018e0dde07f654c22a3171d",
+    UniswapV3TwapPriceOracleV2_Uniswap_3000_USDC:
+      "0x204541bdea985113b68dad86bf67fbbd52829f8984b6f17f6271bcec203161d1",
     UniswapV3TwapPriceOracleV2_Uniswap_10000_USDC:
       "0xc301f891f1f905e68d1c5df5202cf0eec2ee8abcf3a510d5bd00d46f7dea01b4",
     UniswapV3TwapPriceOracleV2:
       "0xc844372c8856a5f9569721d3aca38c7804bae2ae4e296605e683aa8d1601e538", // v1.2.0
-    YVaultV1PriceOracle:
+    YVaultV1PriceOracleV1: // fuse-contracts@v1.0.0
       "0xd0dda181a4eb699a966b23edb883cff43377297439822b1b0f99b06af2002cc3",
+    YVaultV1PriceOracleV2: // fuse-contracts@v1.2.1
+      "0x78ac4b231a4ce3ac5259847cd1cb227bf45882d736722290bee6b6c99a722f22",
     YVaultV2PriceOracle:
       "0x177c22cc7d05280cea84a36782303d17246783be7b8c0b6f9731bb9002ffcc68",
     MasterPriceOracleV1: // fuse-contracts@v1.0.0
       "0xfa1349af05af40ffb5e66605a209dbbdc8355ba7dda76b2be10bafdf5ffd1dc6",
-    MasterPriceOracleV2: // fuse-contracts@v1.2.0
+    MasterPriceOracleV2: // fuse-contracts@80c79b45bda4151e22358d22cc0bf1489f34900c (before final release of v1.2.0)
       "0xdfa5aa37efea3b16d143a12c4ae7006f3e29768b3e375b59842c7ecd3809f1d1",
-    MasterPriceOracleV3: // fuse-contracts@v1.2.1
+    MasterPriceOracleV3: // fuse-contracts@v1.2.0
       "0xe4199a03b164ca492d19d655b85fdf8cc14cf2da6ddedd236712552b7676b03d",
     CurveLpTokenPriceOracle:
       "0x6742ae836b1f7df0cfd9b858c89d89da3ee814c28c5ee9709a371bcf9dfd2145",
@@ -174,8 +189,32 @@ export default class Fuse {
       "0x136d369f53594c2f10e3ff3f14eaaf0bada4a63964f3cfeda3923e3531e407dc",
     UniswapTwapPriceOracleV2_SushiSwap_DAI:
       "0xb4d279232ab52a2fcaee6dc47db486a733c24a499ade9d7de1b0d417d4730817",
+    UniswapTwapPriceOracleV2_SushiSwap_CRV:
+      "0x9df749314d6494a785bb5ff7a5fab25adadb772e10d58b7f692028cc23e2cbb3",
+    UniswapTwapPriceOracleV2_SushiSwap_USDC:
+      "0x2219b54a3e2c36b8b1eca8d511392eacace73a3e1cb55c97dd495f5e47024ba6",
+    UniswapTwapPriceOracleV2_Uniswap_FRAX:
+      "0xc884332403a6234bbb5e860fa27bcf69389b7e372b20045af687d23426e654e3",
+    UniswapTwapPriceOracleV2_SushiSwap_ETH:
+      "0xea501eef0ca55dc6a8360a5a1274895d6dc245dd0ae8cffbff3a14c6624711f0",
     SushiBarPriceOracle:
       "0x3736e8b6c11fcd413c0b60c3291a3a2e2ebe496a2780f3c45790a123f5ee9705",
+    BadgerPriceOracle:
+      "0x310210400b2d3992dc8fb9ace5001b1b55d3a468fba18ae0bc82a375fd150638",
+    HarvestPriceOracleV1: // fuse-contracts@v1.1.4
+      "0x6e23380d1d640118cf80cf2bfa9ca7a89068659dfcb50abc0a7f8b9e5f9daab7",
+    HarvestPriceOracleV2: // fuse-contracts@v1.2.1
+      "0x5eff948725404a38311ebe4b3bafc312f63dd8ae611e3ddcdfebb9cfa231988c",
+    StakedSdtPriceOracle:
+      "0x1b489bd00e5cbe4998e985f147297c1a39bd9da659e78544c94c1f3415edf7b7",
+    TokemakPoolTAssetPriceOracle:
+      "0xc820466d7af2319646d25e2203187254a37cb9b9ae6c8a263d40fb5c01a54c51",
+    MStablePriceOracle:
+      "0x39fc7b2cdac3d401ea91becf897346b2156dbe261162de14082e856103456eb4",
+    StakedSpellPriceOracle:
+      "0x9fcea6d23c7e2e330e35e303a49f39e0c2c783e6b770ccc2de41fbbfbfc539e7",
+    CurveTriCryptoLpTokenPriceOracle:
+      "0x92014d914370d8c59082044786d9b056ea188a95891778c555209c210850d5ae",
   };
 
   static ORACLES = [
