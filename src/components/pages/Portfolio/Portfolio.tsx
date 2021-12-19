1f56ed09964d7b2a3f0d821fa5153e652d70ab62
nathanhleung <github@nathanhleung.com>
2021-12-18 21:03:14 -0500

    wip

--- src/components/pages/Portfolio/Portfolio.tsx
+++ src/components/pages/Portfolio/Portfolio.tsx
@@ -1,7 +1,9 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 import {
   Box,
   Flex,
   Heading,
+  Link,
   Table,
   Tbody,
   Td,
@@ -12,6 +14,10 @@ import {
 } from "@chakra-ui/react";
 import { Column } from "utils/chakraUtils";
 import { sumBy } from "lodash";
+import { useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { FaCaretDown, FaCaretRight } from "react-icons/fa";
+import { Link as RouterLink } from "react-router-dom";
 
 import DashboardBox from "components/shared/DashboardBox";
 import Footer from "components/shared/Footer";
@@ -19,12 +25,84 @@ import { Header } from "components/shared/Header";
 
 import { useRari } from "context/RariContext";
 
-import { useFusePools } from "hooks/fuse/useFusePools";
+import { useFusePools, MergedPool } from "hooks/fuse/useFusePools";
 import { useIsSmallScreen } from "hooks/useIsSmallScreen";
 import { useFusePoolsData } from "hooks/useFusePoolData";
 import { FusePoolData } from "utils/fetchFusePoolData";
 import { smallUsdFormatter } from "utils/bigUtils";
-import { useTranslation } from "react-i18next";
+
+const borderBottomColor = "rgba(255,255,255,0.1)";
+const thStyle: React.CSSProperties = {
+  borderBottomColor,
+  color: "rgba(255,255,255,0.5)",
+  textTransform: "none",
+  fontWeight: "normal",
+  paddingLeft: 0,
+  fontSize: "initial",
+  letterSpacing: 0,
+};
+
+const PoolTr = ({
+  pool,
+  poolData,
+}: {
+  pool: MergedPool;
+  poolData: FusePoolData;
+}) => {
+  const [dropdownOpened, setDropdownOpened] = useState(false);
+  const tdStyle: React.CSSProperties = {
+    color: "white",
+    paddingLeft: 0,
+    borderBottomColor: dropdownOpened ? "transparent" : borderBottomColor,
+  };
+
+  return (
+    <>
+      <Tr onClick={() => setDropdownOpened(!dropdownOpened)}>
+        <Td style={tdStyle}>
+          <Flex alignItems="center">
+            {dropdownOpened ? (
+              <FaCaretDown cursor="pointer" />
+            ) : (
+              <FaCaretRight cursor="pointer" />
+            )}
+            &nbsp;
+            {/* Prevent selection on double click */}
+            <Text fontWeight="bold" userSelect="none">
+              {pool.name}
+            </Text>
+          </Flex>
+        </Td>
+        <Td style={tdStyle}>
+          {smallUsdFormatter(poolData?.totalSupplyBalanceUSD)}
+        </Td>
+        <Td style={tdStyle}>
+          {smallUsdFormatter(poolData?.totalBorrowBalanceUSD)}
+        </Td>
+        <Td style={tdStyle}>
+          <Link
+            as={RouterLink}
+            width="100%"
+            className="no-underline"
+            to={"/fuse/pool/" + pool.id}
+          >
+            <ExternalLinkIcon />
+          </Link>
+        </Td>
+      </Tr>
+      {dropdownOpened && (
+        <Tr>
+          <Td colSpan={99} pt={0} pl={0} borderBottomColor={borderBottomColor}>
+            <DashboardBox width="100%" p={4}>
+              <Text fontWeight="bold">Supply positions</Text>
+              <Text fontWeight="bold">Borrow positions</Text>
+            </DashboardBox>
+          </Td>
+        </Tr>
+      )}
+    </>
+  );
+};
 
 const PortfolioPage = () => {
   const { isAuthed } = useRari();
@@ -32,32 +110,20 @@ const PortfolioPage = () => {
   const { t } = useTranslation();
 
   const { filteredPools: myPools } = useFusePools("my-pools");
-  const poolIds: number[] = myPools?.map(({ id }) => id) ?? [];
-  const fusePoolsData: FusePoolData[] | null = useFusePoolsData(poolIds);
-
-  const totalBorrowBalanceUsd = sumBy(
-    fusePoolsData,
-    (it) => it.totalBorrowBalanceUSD
+  const poolIds: number[] = useMemo(
+    () => myPools?.map(({ id }) => id) ?? [],
+    [myPools]
   );
-  const totalSupplyBalanceUsd = sumBy(
-    fusePoolsData,
-    (it) => it.totalSupplyBalanceUSD
+  const fusePoolsData: FusePoolData[] | null = useFusePoolsData(poolIds);
+  const totalBorrowBalanceUsd = useMemo(
+    () => sumBy(fusePoolsData, (it) => it.totalBorrowBalanceUSD),
+    [fusePoolsData]
+  );
+  const totalSupplyBalanceUsd = useMemo(
+    () => sumBy(fusePoolsData, (it) => it.totalSupplyBalanceUSD),
+    [fusePoolsData]
   );
   const netBalanceUsd = totalSupplyBalanceUsd - totalBorrowBalanceUsd;
-  const thStyle: React.CSSProperties = {
-    borderBottomColor: "rgba(255,255,255,0.1)",
-    color: "rgba(255,255,255,0.5)",
-    textTransform: "none",
-    fontWeight: "normal",
-    paddingLeft: 0,
-    fontSize: "initial",
-    letterSpacing: 0,
-  };
-  const tdStyle: React.CSSProperties = {
-    borderBottomColor: "rgba(255,255,255,0.1)",
-    color: "white",
-    paddingLeft: 0,
-  };
 
   return (
     <Column
@@ -98,19 +164,16 @@ const PortfolioPage = () => {
                 <Th style={thStyle}>{t("Fuse Pool")}</Th>
                 <Th style={thStyle}>{t("Supplied")}</Th>
                 <Th style={thStyle}>{t("Borrowed")}</Th>
+                <Th style={thStyle} />
               </Tr>
             </Thead>
             <Tbody>
               {myPools.map((pool, index) => {
                 const fusePoolData = fusePoolsData?.[index];
 
-                return (
-                  <Tr key={pool.id}>
-                    <Td style={tdStyle}>
-                      <Text fontWeight="bold">{pool.name}</Text>
-                    </Td>
-                  </Tr>
-                );
+                return fusePoolData ? (
+                  <PoolTr key={pool.id} pool={pool} poolData={fusePoolData} />
+                ) : null;
               })}
             </Tbody>
           </Table>
